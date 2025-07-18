485441bc94666083426dd48a0428db26a50b8ea0
Sara Kidd <hrpdc@users.noreply.github.com>
2025-07-18 15:14:28 -0400

    Update production.rb
    commented out line 85 for testing (no email)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,5 +82,5 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = Rails.application.secrets.email.symbolize_keys
+  # config.action_mailer.smtp_settings = Rails.application.secrets.email.symbolize_keys
 end
