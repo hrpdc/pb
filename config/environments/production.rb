3a2761caf61530decd91e104b70a84a3c536d7d3
Sara Kidd <hrpdc@users.noreply.github.com>
2025-07-25 10:49:38 -0400

    Update production.rb
    Stop email delivery for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
 
   # Configure mailer.
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_deliveries = false
   config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = Rails.application.secrets.email.symbolize_keys
 end
