52a07112de0e826f472f3afccb9cd06b26475e40
Sara Kidd <hrpdc@users.noreply.github.com>
2025-07-25 13:53:44 -0400

    Update seeds.rb
    Adding a comment to test persistence of image upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+#   Adding a comment to test persistence of image upload
 require 'csv'
 
 # Add a sample election
